ed282eab538e8e1696b110387f5fa5305fd0b715
John Andrei Tacujan <89928907+DreiDev04@users.noreply.github.com>
2024-08-25 16:03:27 +0000

    added: var and mean

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    np_list = np.array(list).reshape(3, 3)
 
-
-
-
-    return calculations
+    return {
+        'mean': [
+    np.mean(np_list, axis=0).tolist(),
+    np.mean(np_list, axis=1).tolist(),
+    np.mean(np_list).tolist()
+    ], 
+'variance': [
+    np.var(np_list, axis=0).tolist(),
+    np.var(np_list, axis=1).tolist(),
+    np.var(np_list).tolist()
+    ]
+    }
